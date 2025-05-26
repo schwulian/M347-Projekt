3ec193c6dc543d98477d3462c01403562f29f235
sib <silvan.brors@ti8m.ch>
2025-05-26 10:36:52 +0200

    initial commit

---
+++ frontend/src/navbar/navbar.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+
+export const Navbar = () => {
+    return (
+        <>
+            <nav className="navbar bg-body-tertiary">
+                <div className="container-fluid">
+                    <span className="navbar-brand mb-0 h1">Navbar</span>
+                </div>
+
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Features</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Pricing</a>
+                </li>
+            </nav>
+        </>
+    )
+}
