890b1c0197223c3c58d07b43251c06f350792d23
sib <silvan.brors@ti8m.ch>
2025-06-23 10:50:01 +0200

    added url to database

--- db/init.sql
+++ db/init.sql
@@ -2,10 +2,11 @@ CREATE TABLE films (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     duration float NOT NULL,
-    regiseur VARCHAR(255) NOT NULL
+    regiseur VARCHAR(255) NOT NULL,
+    film_url VARCHAR(255) NOT NULL
 );
 
-INSERT INTO films (title, duration, regiseur) VALUES
-    ('Inception', 148, 'Christopher Nolan'),
-    ('The Godfather', 175, 'Francis Ford Coppola'),
-    ('Minions', 182, 'Gru der Wahre');
+INSERT INTO films (title, duration, regiseur, film_url) VALUES
+    ('Inception', 148, 'Christopher Nolan', 'https://musicart.xboxlive.com/7/1f4b1200-0000-0000-0000-000000000002/504/image.jpg'),
+    ('The Godfather', 175, 'Francis Ford Coppola', 'https://m.media-amazon.com/images/M/MV5BNGEwYjgwOGQtYjg5ZS00Njc1LTk2ZGEtM2QwZWQ2NjdhZTE5XkEyXkFqcGc@._V1_.jpg'),
+    ('Minions', 182, 'Gru der Wahre', 'https://images.prod.ecom.mueller.de/_next/image/?url=https%3A%2F%2Fstatic.prod.ecom.mueller.de%2Fproducts%2F5053083026356%2Fvideojakob_5053083026356_pv_463075.jpg&w=2880&q=75');
