1bef6d6164b9503b9760991ba710b44f42e3f3cc
jun <Julian.Nagel@ti8m.ch>
2025-06-23 10:31:36 +0200

    Main method isch leer gsi bruh, und han no application properties apasst

--- backend/src/main/java/ch/bbw/M347/Main.java
+++ backend/src/main/java/ch/bbw/M347/Main.java
@@ -1,11 +1,12 @@
 package ch.bbw.M347;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args){
-
+        SpringApplication.run(Main.class, args);
     }
 }
