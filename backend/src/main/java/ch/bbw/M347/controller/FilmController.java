2ac4a06c1b04e4436b3286f81cab08da1734b9ba
jun <Julian.Nagel@ti8m.ch>
2025-06-27 16:34:20 +0200

    More crud functionalities, spring actuator. ich weiss nd wieso ich uf englisch schrib. And tested it if it works with the DB.

--- backend/src/main/java/ch/bbw/M347/controller/FilmController.java
+++ backend/src/main/java/ch/bbw/M347/controller/FilmController.java
@@ -2,24 +2,79 @@ package ch.bbw.M347.controller;
 
 import ch.bbw.M347.model.Film;
 import ch.bbw.M347.repository.FilmRepo;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/Films")
+@RequestMapping("/api")
 public class FilmController {
     private final FilmRepo repo;
 
-    public FilmController(FilmRepo repo){
+    public FilmController(FilmRepo repo) {
         this.repo = repo;
     }
 
-    @PostMapping("/Film")
-    public ResponseEntity<String> createFilm(@RequestBody Film film){
-        Film savedFilm = repo.save(film);
-        return ResponseEntity.ok("Film added");
+    @PostMapping("/film")
+    public ResponseEntity<String> createFilm(
+            @RequestBody Film film
+    ) {
+        repo.save(film);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Film hinzugefügt");
     }
+
+    @GetMapping("/film/{filmId}")
+    public ResponseEntity<String> getFilmById(
+            @PathVariable Long filmId
+    ) {
+        Optional<Film> film = repo.findById(filmId);
+
+        return ResponseEntity.ok("Film: " + film.toString());
+    }
+
+    @GetMapping("/film")
+    public ResponseEntity<List<Film>> getAllFilms() {
+        List<Film> films = repo.findAll();
+        return ResponseEntity.ok(films);
+    }
+
+    @PutMapping("/film/{filmId}")
+    public ResponseEntity<String> updateFilmById(
+            @PathVariable Long filmId,
+            @RequestBody Film newFilm
+    ) {
+        Optional<Film> filmToBeChangedOptional = repo.findById(filmId);
+
+        if (filmToBeChangedOptional.isPresent()) {
+            Film filmToBeChanged = filmToBeChangedOptional.get();
+
+            filmToBeChanged.setRegisseur(newFilm.getRegisseur());
+            filmToBeChanged.setTitle(newFilm.getTitle());
+            filmToBeChanged.setDuration(newFilm.getDuration());
+            filmToBeChanged.setReleaseDate(newFilm.getReleaseDate());
+
+            repo.save(filmToBeChanged);
+
+            return ResponseEntity.ok("tmm");
+        } else {
+            return ResponseEntity.status(404).body("nld gfunde");
+        }
+    }
+
+    @DeleteMapping("film/{filmId}")
+    public ResponseEntity<String> deleteById(
+            @PathVariable Long filmId
+    ) {
+        if (repo.existsById(filmId)) {
+            repo.deleteById(filmId);
+            return ResponseEntity.ok("deleted");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("not found");
+        }
+    }
+
+
 }
--- backend/src/main/java/ch/bbw/M347/model/Film.java
+++ backend/src/main/java/ch/bbw/M347/model/Film.java
@@ -12,7 +12,7 @@ public class Film {
 
     private String title;
     private String regisseur;
-    private float duration;
+    private float duration; //minuten
     private int releaseDate;
 
     public Film() {}
@@ -22,5 +22,31 @@ public class Film {
         this.regisseur = regisseur;
         this.duration = duration;
         this.releaseDate = releaseDate;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getRegisseur() { return regisseur; }
+    public void setRegisseur(String regisseur) { this.regisseur = regisseur; }
+
+    public float getDuration() { return duration; }
+    public void setDuration(float duration) { this.duration = duration; }
+
+    public int getReleaseDate() { return releaseDate; }
+    public void setReleaseDate(int releaseDate) { this.releaseDate = releaseDate; }
+
+    @Override
+    public String toString() {
+        return "Film{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", regisseur='" + regisseur + '\'' +
+                ", duration=" + duration +
+                ", releaseDate=" + releaseDate +
+                '}';
     }
 }
